58fc3aa9e1d94b63b57557c824a805f36656dc0a
John Michael <johnny25580@gmail.com>
2023-10-01 12:31:24 +0100

    OtpResponseDto.java

--- src/main/java/com/johnny/otpcoreservice/Dto/response/OtpResponseDto.java
+++ src/main/java/com/johnny/otpcoreservice/Dto/response/OtpResponseDto.java
@@ -1,2 +1,21 @@
-package com.johnny.otpcoreservice.Dto.response;public class OtpResponseDto {
+package com.johnny.otpcoreservice.Dto.response;
+
+import com.johnny.otpcoreservice.domain.OtpRequest;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OtpResponseDto implements Serializable{
+
+
+    private String otp;
+
+    private String userId;
+
+    private int ttl;
 }
