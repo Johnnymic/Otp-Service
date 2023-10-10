2b50ffa94d3ad6c453fc33bc06f97feb391ada28
John Michael <johnny25580@gmail.com>
2023-10-10 12:59:56 +0100

    criteria added

--- src/main/java/com/johnny/otpcoreservice/services/criteria/OtpCriteria.java
+++ src/main/java/com/johnny/otpcoreservice/services/criteria/OtpCriteria.java
@@ -1,2 +1,22 @@
-package com.johnny.otpcoreservice.services.criteria;public class OtpCriteria {
+package com.johnny.otpcoreservice.services.criteria;
+
+import com.johnny.otpcoreservice.domain.enumeration.OtpType;
+import lombok.*;
+
+import java.time.Instant;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OtpCriteria {
+
+    private String otp;
+    private String otpType;
+    private String userId;
+    private Integer ttl;
+    private Integer otpLength;
+    private Instant dateCreated;
+    private Instant dateValidated;
+    private OtpType status;
 }
