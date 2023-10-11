5c38848abefad12c096a373d67ce21e712c2b124
John Michael <johnny25580@gmail.com>
2023-10-11 13:00:58 +0100

    OTPservice added

--- src/main/java/com/johnny/otpcoreservice/services/OtpService.java
+++ src/main/java/com/johnny/otpcoreservice/services/OtpService.java
@@ -1,2 +1,25 @@
-package com.johnny.otpcoreservice.services;public interface OtpService {
+package com.johnny.otpcoreservice.services;
+
+import com.johnny.otpcoreservice.Dto.request.OtpRequestDto;
+import com.johnny.otpcoreservice.Dto.response.OtpResponseDto;
+import com.johnny.otpcoreservice.services.criteria.OtpCriteria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OtpService {
+
+     OtpResponseDto saveOtp(OtpRequestDto otpRequestDto);
+
+    Page<OtpResponseDto> findAll(Pageable pageable);
+
+    Optional<OtpResponseDto> findOne(Long id);
+
+    List<OtpRequestDto> findByCriteria(OtpCriteria criteria);
+
+    Page<OtpRequestDto> findPageByCriteria(OtpCriteria criteria,Pageable pageable);
+
+    void deleteOtp(Long id);
 }
