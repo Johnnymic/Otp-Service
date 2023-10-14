43aa4c349acb9627eb368180cffaf1772b9aeb3d
John Michael <johnny25580@gmail.com>
2023-10-14 13:06:49 +0100

    added

--- src/main/java/com/johnny/otpcoreservice/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/johnny/otpcoreservice/exceptions/ResourceNotFoundException.java
@@ -1,2 +1,8 @@
-package com.johnny.otpcoreservice.exceptions;public class ResourceNotFoundException {
+package com.johnny.otpcoreservice.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
