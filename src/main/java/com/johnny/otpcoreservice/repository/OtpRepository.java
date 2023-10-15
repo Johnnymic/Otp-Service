c1d359d0a34d69a24a5e1b08c3f19212b8ebea0e
John Michael <johnny25580@gmail.com>
2023-10-15 13:10:00 +0100

    added

--- src/main/java/com/johnny/otpcoreservice/repository/OtpRepository.java
+++ src/main/java/com/johnny/otpcoreservice/repository/OtpRepository.java
@@ -1,2 +1,15 @@
-package com.johnny.otpcoreservice.repository;public interface OtpRepository {
+package com.johnny.otpcoreservice.repository;
+
+import com.johnny.otpcoreservice.Dto.request.OtpRequestDto;
+import com.johnny.otpcoreservice.Dto.response.OtpResponseDto;
+import com.johnny.otpcoreservice.domain.OtpRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+public interface OtpRepository extends JpaRepository<OtpRequest, Long> , JpaSpecificationExecutor<OtpRequest> {
+
+
 }
