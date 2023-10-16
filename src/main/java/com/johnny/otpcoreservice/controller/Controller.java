a8a100bc154153959377d2d45b55bd12fdb6a9a1
John Michael <johnny25580@gmail.com>
2023-10-16 13:18:04 +0100

    otp controller added

--- src/main/java/com/johnny/otpcoreservice/controller/Controller.java
+++ src/main/java/com/johnny/otpcoreservice/controller/Controller.java
@@ -1,2 +1,37 @@
-package com.johnny.otpcoreservice.controller;public class Controller {
+package com.johnny.otpcoreservice.controller;
+
+import com.johnny.otpcoreservice.Dto.request.OtpRequestDto;
+import com.johnny.otpcoreservice.Dto.response.OtpResponseDto;
+
+
+import com.johnny.otpcoreservice.services.impl.ExtendedOtpService;
+import com.johnny.otpcoreservice.utils.ApiResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/otp")
+@RequiredArgsConstructor
+@Slf4j
+public class Controller {
+
+   private final ExtendedOtpService extendedOtpService;
+    @PostMapping()
+    public ResponseEntity<ApiResponse> getOtp(@Valid @RequestBody OtpRequestDto otpRequest){
+        ApiResponse apiResponse = extendedOtpService.getOtp(otpRequest);
+        return  ResponseEntity.ok(apiResponse);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ApiResponse> isOtpValid(@Valid @RequestBody OtpResponseDto otpResponseDto){
+        log.info("otp {}", otpResponseDto);
+        ApiResponse apiResponse = extendedOtpService.isOtpValid(otpResponseDto);
+        return  ResponseEntity.ok(apiResponse);
+    }
+
 }
